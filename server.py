b4db9effe2f50e320c0d778a60290f9de863c45a
HuangJyunYu <yillkid@gmail.com>
2024-03-13 02:41:52 +0800

    Update server.py

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def hello():
 
 if __name__ == "__main__":
     app.debug = False
-    app.run(host = "0.0.0.0", port = os.getenv("SERVICE_PORT")
+    app.run(host = "0.0.0.0", port = os.getenv("SERVICE_PORT"))
